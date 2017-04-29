df9314faa76f5591b3680ee6f5e4ee8ab5f28fc1
xinq <xeniaqian94@gmail.com>
2017-04-29 14:26:26 -0400

    Update crfpp tool

---
+++ crfpp-master/crf_learn.cpp
@@ -0,0 +1,17 @@
+//
+//  CRF++ -- Yet Another CRF toolkit
+//
+//  $Id: crf_learn.cpp 1587 2007-02-12 09:00:36Z taku $;
+//
+//  Copyright(C) 2005-2007 Taku Kudo <taku@chasen.org>
+//
+#include "crfpp.h"
+
+#ifdef _WIN32
+#include "winmain.h"
+#endif
+
+
+int main(int argc, char **argv) {
+  return crfpp_learn(argc, argv);
+}
---
+++ crfpp-master/crf_test.cpp
@@ -0,0 +1,15 @@
+//
+//  CRF++ -- Yet Another CRF toolkit
+//
+//  $Id: crf_test.cpp 1587 2007-02-12 09:00:36Z taku $;
+//
+//  Copyright(C) 2005-2007 Taku Kudo <taku@chasen.org>
+//
+#include "crfpp.h"
+#ifdef _WIN32
+#include "winmain.h"
+#endif
+
+int main(int argc, char **argv) {
+  return crfpp_test(argc, argv);
+}
